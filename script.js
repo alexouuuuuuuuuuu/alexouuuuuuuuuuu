4aa444a76794d8d106d76da813a1e3f4c5881be2
alexouuuuuuuuuuu <alexis.maugain@etu.unilim.fr>
2024-12-20 11:20:01 +0100

    Co-authored-by: Louka <Loukkka@users.noreply.github.com> Co-authored-by: Arda <arda-gunduz@users.noreply.github.com>

--- script.js
+++ script.js
@@ -30,26 +30,49 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 
     // Animation des boules dans la section 7
-    gsap.utils.toArray(".section7__container-boules").forEach((element) => {
+    gsap.utils.toArray(".section7__container-boules").forEach((element, index) => {
+
         gsap.fromTo(element, {
-            x: "0%"
+            x: "-350px"
         }, {
-            x: "80px",
-            duration: 1,
-            yoyo: true,
-            repeat: false,
-            ease: "power2.out",
+            x: "300px", 
+            duration: 1 * (index + 1), 
+            repeat: 0, 
+            ease: "power2.out", 
             scrollTrigger: {
                 trigger: element,
-                start: "top 80%",
+                start: "top 90%",
                 end: "top 20%",
                 scrub: true,
-                markers: false
+                markers: false, 
             }
         });
     });
 
+    const marionnette1 = document.querySelector('img[src="./assets/marionnette1.svg"]');
+    const marionnette2 = document.querySelector('img[src="./assets/marionnette2.svg"]');
+
+    gsap.set(marionnette2, { opacity: 0 });
+
+    const tl = gsap.timeline({ defaults: { duration: 0.1 } }); // Set duration to 0 for instant animation
+    tl.to(marionnette1, { opacity: 0 })
+        .to(marionnette2, { opacity: 1 });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    ScrollTrigger.create({
+        trigger: marionnette1,
+        start: "top center",
+        end: "bottom center",
+        animation: tl,
+        scrub: true,
+    });
 });
+
+
+
+
+
 
     // Animation des boules dans la section 7
 
